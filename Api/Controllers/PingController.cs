405ecbb01ef24b19cad06c5f974fee6ccfd8b174
Nicklas Rohman <Nicklas.Rohman@nexergroup.com>
2025-02-07 15:43:44 +0100

    Setup a basic API

---
+++ Api/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet(Name = "ping")]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
